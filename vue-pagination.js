20c0cd8622847d2c828fcfbbee7074f71a24255a
wumengqiang <dean@visualbusiness.com>
2016-12-08 19:03:28 +0800

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   vue-pagination.css
    	new file:   vue-pagination.js
    	new file:   vue-pagination.scss

---
+++ vue-pagination.js
@@ -0,0 +1,37 @@
+.pagination-sm {
+  display: inline-block;
+  float: right;
+  margin-top: 15px;
+  font-size: 14px;
+  color: #445FA8;
+  -webkit-user-select: none; }
+  .pagination-sm .pagination-prev,
+  .pagination-sm .pagination-next,
+  .pagination-sm .pagination-item {
+    display: inline-block;
+    list-style: none;
+    cursor: pointer;
+    border: 1px solid #e5e5e5;
+    background-color: white;
+    padding: 5px 14px;
+    margin-right: 9px;
+    line-height: 26px; }
+    .pagination-sm .pagination-prev.active, .pagination-sm .pagination-prev:hover,
+    .pagination-sm .pagination-next.active,
+    .pagination-sm .pagination-next:hover,
+    .pagination-sm .pagination-item.active,
+    .pagination-sm .pagination-item:hover {
+      background: #445FA8;
+      border: 1px solid transparent;
+      color: #FFFFFF; }
+    .pagination-sm .pagination-prev.disable:hover,
+    .pagination-sm .pagination-next.disable:hover,
+    .pagination-sm .pagination-item.disable:hover {
+      background-color: white;
+      color: #707070;
+      border: 1px solid #e5e5e5; }
+    .pagination-sm .pagination-prev.disable,
+    .pagination-sm .pagination-next.disable,
+    .pagination-sm .pagination-item.disable {
+      cursor: not-allowed;
+      color: #707070; }
